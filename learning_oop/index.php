81991fe80c74ccde7430e55861917d92432e0925
VaishaliPandiyen <81515185+VaishaliPandiyen@users.noreply.github.com>
2024-03-11 16:22:30 +0000

    .

--- learning_oop/index.php
+++ learning_oop/index.php
@@ -5,6 +5,8 @@ camelCase class names and make it singular
 
 <?php
 
+require_once("vendor_class.php");
+
 get_declared_classes(); // this gives us an array of declared classes (and built-in ones)
 
 class_exists(''); // whether a class is in that array
@@ -15,13 +17,22 @@ $vendor1 = new Vendor;
 
 $vendor1->name = 'Anna';
 
-get_class($vendor1); // this gives us the name of the defined class i.e. Vendor here
+echo get_class($vendor1). "<br/>"; // this gives us the name of the defined class i.e. Vendor here
 
-is_array($vendor1, 'Vendor'); // whether the instance is made of a Vendor class or not
+echo is_a($vendor1, 'Vendor'). "<br/>"; // whether the instance is made of a Vendor class or not
 
-get_class_vars('Vendor'); // this gives us the variables for the defined class i.e. Vendor here
+print_r(get_class_vars('Vendor')); // this gives us the variables for the defined class i.e. Vendor here
+//get_class_methods('Vendor'); gives methods/functions for Vendor
+echo "<br/>";
 
-get_object_vars($vendor1); // this gives us the set of variables for object/instance i.e. vendor1 here
+print_r(get_object_vars($vendor1)). "<br/>"; // this gives us the set of variables for object/instance i.e. vendor1 here
+echo "<br/>";
 
 if (property_exists('Vendor', 'first_name')) {
-}
+}// method_exists('Vendor', 'say_name')
+
+$vendor1->say_name();
+
+$vendor1->location = "Canton, Cardiff";
+
+print_r($vendor1->add_items(['Lemon', 'Apple', 'Plums']));
--- learning_oop/vendor_class.php
+++ learning_oop/vendor_class.php
@@ -3,5 +3,18 @@
 class Vendor
 {
     var $name;
+    var $location;
+    var $items = [];
+
+    function say_name() {
+        return 'Vendor name is '. $this->name;
+    }
+
+    function add_items($arr) {
+        foreach ($arr as $item) {
+            $this->items[] = $item;
+        }
+        return $this->items;
+    }
 
 }
