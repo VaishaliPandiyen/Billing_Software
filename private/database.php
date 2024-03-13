df29ee45290ba997736fe93990043948aeb1573c
VaishaliPandiyen <81515185+VaishaliPandiyen@users.noreply.github.com>
2024-03-13 20:29:46 +0000

    .

--- private/database.php
+++ private/database.php
@@ -4,11 +4,13 @@ require_once('db_credentials.php');
 
 // this will be used or called in initialise.php
 function db_connect() {
-    $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME, 3300);
     /* !IMPORTANT:
     Since my local system config uses custom port 3300 instead of default 3306, I updated it in my.ini in MAMP and added it here too.
     I did this when installing MySQL on this device as 306 wasn't available
     */ 
+    $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME, 3300);
+    // ALTERNATE: OOP verision: 
+    // $connection = new mysqli(DB_SERVER, DB_USER, DB_PASS, DB_NAME, 3300);
     confirm_connection();
     return $connection;
 }
