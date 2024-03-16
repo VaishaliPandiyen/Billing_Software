f794732bd6bb6d59ac0fe33b4ee96a61a95afbc4
VaishaliPandiyen <81515185+VaishaliPandiyen@users.noreply.github.com>
2024-03-16 03:11:17 +0000

    + pagination; need COMMENTS

--- public/user_staff/invoices/index.php
+++ public/user_staff/invoices/index.php
@@ -1,9 +1,21 @@
-<?php require_once("../../../private/initialise.php");
+<?php require_once ("../../../private/initialise.php");
 
+// In URL, index.php?page=2 will be there for >1st page
+$curr_pg = $_GET['page'] ?? 1;
+$per_pg = 15;
+$total;
+// $total = Invoice::count_all();
+
+$pagination = new Pagination($curr_pg, $per_pg, $total);
 $invoices = all_invoices();
 
+// $sql = "SELECT * FROM invoices ";
+// $sql .= "LIMIT {$per_pg} ";
+// $sql .= "OFFSET {$pagination->offset()} ";
+// $invoices = Invoice::find_sql();
+
 $page_title = "Invoices";
-include(SHARED_PATH . "/staff_header.php");
+include (SHARED_PATH . "/staff_header.php");
 ?>
 <div class="content">
   <div class="vendors listing">
@@ -23,24 +35,27 @@ include(SHARED_PATH . "/staff_header.php");
 
       <?php
       if ($invoices) {
-          while ($i = $invoices->fetch_assoc()) {
-      ?>
-      <tr>
-         <td><?php echo h($i['i_id']); ?></td>
-         <td><?php echo h($i['i_date']); ?></td>
-         <td><?php echo h($i['i_mode']); ?></td>
-         <td><?php echo h($i['i_total']); ?></td>
-         <td><a class="action" href="<?php echo url_for("/user_staff/invoices/show.php?id=" . h(u($i['i_id']))); ?>">View</a></td>
-         <td><a class="action" href="<?php echo url_for("/user_staff/invoices/suggest_edit.php?id=" . h(u($i['i_id']))); ?>">Suggest Edit</a></td>
-      </tr>
-      <?php }
+        while ($i = $invoices->fetch_assoc()) {
+          ?>
+                                                      <tr>
+                                                         <td><?php echo h($i['i_id']); ?></td>
+                                                         <td><?php echo h($i['i_date']); ?></td>
+                                                         <td><?php echo h($i['i_mode']); ?></td>
+                                                         <td><?php echo h($i['i_total']); ?></td>
+                                                         <td><a class="action" href="<?php echo url_for("/user_staff/invoices/show.php?id=" . h(u($i['i_id']))); ?>">View</a></td>
+                                                         <td><a class="action" href="<?php echo url_for("/user_staff/invoices/suggest_edit.php?id=" . h(u($i['i_id']))); ?>">Suggest Edit</a></td>
+                                                      </tr>
+                              <?php }
       } else {
-          echo "Error: " . $db->error;
+        echo "Error: " . $db->error;
       } ?>
       </table>
-
+    <?php
+    $url = url_for("/user_staff/invoices/index.php");
+    echo $pagination->page_links($url);
+    ?>
   </div>
 
 </div>
-<?php include(SHARED_PATH . "/staff_footer.php");
+<?php include (SHARED_PATH . "/staff_footer.php");
 ?>
